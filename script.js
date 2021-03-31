02e01604093d20fe167411cc3eb1c8aa4bf1c9f0
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-31 17:03:41 -0400

    working on rock

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
+let resultsParagraph = document.getElementById('results');
+
+let rockButton = document.getElementsByClassName("rockButton");
+let paperButton = document.getElementsByClassName("paperButton");
+let scissorsButton = document.getElementsByClassName("scissorsButton");
+
 function computerPlay() {
     const initialRandom = Math.random();
     const multiplied = initialRandom * 3;
     const answer = Math.floor(multiplied);
     if (answer === 0) {
-        console.log("Rock");
+        // console.log("Rock");
+        const Rock = "Rock";
+        return Rock;
     } else if (answer === 1) {
-        console.log("Paper");
+        // console.log("Paper");
+        const Paper = "Paper";
+        return Paper;
     } else if (answer === 2) {
-        console.log("Scissors");
+        // console.log("Scissors");
+        const Scissors = "Scissors";
+        return Scissors;
     }
 }
 
-computerPlay();
+function playerClickedRock() {
+    let computerSelection = computerPlay();
+    // console.log(computerSelection);
+    if (computerSelection === "Rock") {
+        resultsParagraph.innerHTML = "Draw";
+        // return "Draw";
+    } else if (computerSelection === "Paper") {
+        // return "You Lose! Paper beats Rock";
+        resultsParagraph.innerHTML = "You Lose! Paper beats Rock";
+    } else if (computerSelection === "Scissors") {
+        // return "You Win! Rock beats Scissors";
+        resultsParagraph.innerHTML = "You Win! Rock beats Scissors";
+    }
+
+}
+
