2b0c4d63dd6f993521649a9ae78c66e2e1e75f66
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-31 17:11:18 -0400

    working on Scissors

--- script.js
+++ script.js
@@ -27,24 +27,35 @@ function playerClickedRock() {
     let computerSelection = computerPlay();
     // console.log(computerSelection);
     if (computerSelection === "Rock") {
-        resultsParagraph.innerHTML = "Draw";
+        resultsParagraph.innerHTML = "Rock: Draw";
         // return "Draw";
     } else if (computerSelection === "Paper") {
         // return "You Lose! Paper beats Rock";
-        resultsParagraph.innerHTML = "You Lose! Paper beats Rock";
+        resultsParagraph.innerHTML = "Paper: You Lose! Paper beats Rock";
     } else if (computerSelection === "Scissors") {
         // return "You Win! Rock beats Scissors";
-        resultsParagraph.innerHTML = "You Win! Rock beats Scissors";
+        resultsParagraph.innerHTML = "Scissors: You Win! Rock beats Scissors";
     }
 }
 
 function playerClickedPaper() {
     let computerSelection = computerPlay();
     if (computerSelection === "Rock") {
-        resultsParagraph.innerHTML = "You Win! Paper beats Rock";
+        resultsParagraph.innerHTML = "Rock: You Win! Paper beats Rock";
     } else if (computerSelection === "Paper") {
-        resultsParagraph.innerHTML = "Draw";
+        resultsParagraph.innerHTML = "Paper: Draw";
     } else if (computerSelection === "Scissors") {
-        resultsParagraph.innerHTML = "You Lose! Scissors beats Paper";
+        resultsParagraph.innerHTML = "Scissors: You Lose! Scissors beats Paper";
+    }
+}
+
+function playerClickedScissors() {
+    let computerSelection = computerPlay();
+    if (computerSelection === "Rock") {
+        resultsParagraph.innerHTML = "Rock: You Lose! Rock beats Scissors";
+    } else if (computerSelection === "Paper") {
+        resultsParagraph.innerHTML = "Paper: You Win! Scissors beats Paper";
+    } else if (computerSelection === "Scissors") {
+        resultsParagraph.innerHTML = "Scissors: Draw";
     }
 }
